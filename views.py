f6b8e98c86a39224dc4a7262697de93e0be74eb3
Laraalmo <46568038+Laraalmo@users.noreply.github.com>
2019-06-08 04:12:41 +0300

    Add files via upload

--- views.py
+++ views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render
 from .models import *
+from django.urls import *
+
 # Create your views here.
 def index(request):
     return render(request,'Welcome to our community',context)
 
+def user(request):
+    sign=User.object.all()
+    return render(request,"Join and have fun")
 
 def listSkills (request):
     Skills=Skill.object.all()
 
+    return render(request,"The List")
 
+
+def showProfile (reqest):
+    Volunteers=Volunteer.object.all()
+
+    return render(reqest,"Hi this is ..")
+
+def comment (request):
+    Comments=Comment.object.all()
+
+    return render(request,"Leave your comment")
 
